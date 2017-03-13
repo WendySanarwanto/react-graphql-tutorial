5656410d4cf53a5d00a6379fb94488fc3a8e0ed0
WendySanarwanto <wendy.sanarwanto@gmail.com>
2017-03-14 07:29:42 +0800

    Wrap App component with Apollo Client component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import './App.css';
 
 import { ChannelsList } from './components';
 
+import { ApolloClient, ApolloProvider } from 'react-apollo';
+
+const apolloClient = new ApolloClient();
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Apollo</h2>
+      <ApolloProvider client={apolloClient}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Apollo</h2>
+          </div>
+          <p className="App-intro">
+            <ChannelsList />
+          </p>
         </div>
-        <p className="App-intro">
-          <ChannelsList />
-        </p>
-      </div>
+      </ApolloProvider>
     );
   }
 }
